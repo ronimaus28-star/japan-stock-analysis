ca608175438be144d6d798399867eb034384a6d6
ronimaus28-star <ronimaus28@gmail.com>
2026-05-02 12:57:22 +0800

    Create stock_analysis.py
    Initial Japanese stock analysis script

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,1 +1,15 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
 
+ticker = "7203.T"  # 丰田汽车
+
+stock = yf.download(ticker, period="6mo")
+
+print(stock.tail())
+
+stock["Close"].plot(figsize=(12, 6))
+plt.title("Toyota Stock Price (6 Months)")
+plt.xlabel("Date")
+plt.ylabel("Price (JPY)")
+plt.grid(True)
+plt.show()
